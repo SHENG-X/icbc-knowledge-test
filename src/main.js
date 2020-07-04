544124fb6234f3a9070c10b0aa88281f713e71ed
Sheng Xiao <sheng26v@gmail.com>
2020-07-04 15:07:27 -0700

    chore: globally registered vue notification component

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default.css';
+import Notifications from 'vue-notification';
 
 import App from './App.vue';
 import router from './router';
 
 Vue.use(VueMaterial);
+Vue.use(Notifications);
 
 Vue.config.productionTip = false;
 
