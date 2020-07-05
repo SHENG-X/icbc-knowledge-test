1ef0eaaaf0a00f90d0748769c1a84b283bec2a55
Sheng Xiao <sheng26v@gmail.com>
2020-07-04 17:27:35 -0700

    feat: initial home page layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,48 @@
 <template>
-  <h1>
-    Home page
-  </h1>
+  <div>
+    <home-route-card
+      title="All Questions"
+      description="These are all the questions that we have.
+        It is important for you understand all the questions
+        before you take the knowledge test."
+      buttonTitle="Practice All Quesions"
+      route="/all"
+    />
+    <home-route-card
+      title="Mock Exam"
+      description="In order to pass the exam, you’ll need to get 40 out of 50 questions
+        right on a multiple-choice knowledge test. Everytime you retake the exame the
+        questions will be different."
+      buttonTitle="Start Mock Test"
+      route="/mock"
+    />
+    <home-route-card
+      title="Road Sign Quizzes"
+      description="The blow quiz sets will help you learn and practice knowledge
+        of road signs.On the actual written test, you are reuqired to correctly
+        answer at least 90% of the questions."
+        buttonTitle="Practice Sign Quesions"
+        route="/signs"
+    />
+    <home-route-card
+      title="Road Rule Quizzes"
+      description="The blow quiz sets will help you learn and practice knowledge
+        of road rules.On the actual written test, you are reuqired to correctly
+        answer at least 90% of the questions."
+        buttonTitle="Practice Rule Quesions"
+        route="/rules"
+    />
+  </div>
 </template>
 
 <script>
+import HomeRouteCard from '@/components/HomeRouteCard.vue';
+
 export default {
+  components: {
+    HomeRouteCard,
+  },
+
 };
 </script>
 
