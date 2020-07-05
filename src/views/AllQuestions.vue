6cdaf6c74c3cf4e32dc092a98cc4b794750d64f1
Sheng Xiao <sheng26v@gmail.com>
2020-07-04 17:24:46 -0700

    feat: all questions and mock questions components

---
+++ src/views/AllQuestions.vue
@@ -0,0 +1,20 @@
+<template>
+  <test-panel
+    title="All Questions"
+    description="These are all the questions that we have.
+      It is important for you understand all the questions
+      before you take the knowledge test."
+    :questions="$store.getters.allQuestions"
+    oneTestTitle="Practice All Quesions"
+  />
+</template>
+
+<script>
+import TestPanel from '@/components/TestPanel.vue';
+
+export default {
+  components: {
+    TestPanel,
+  },
+};
+</script>
---
+++ src/views/MockQuestions.vue
@@ -0,0 +1,20 @@
+<template>
+  <test-panel
+    title="Mock Exam"
+    description="In order to pass the exam, you’ll need to get 40 out of 50 questions
+      right on a multiple-choice knowledge test. Everytime you retake the exame the
+      questions will be different."
+    :questions="$store.getters.mockQuestions"
+    oneTestTitle="Start Mock Test"
+  />
+</template>
+
+<script>
+import TestPanel from '@/components/TestPanel.vue';
+
+export default {
+  components: {
+    TestPanel,
+  },
+};
+</script>
