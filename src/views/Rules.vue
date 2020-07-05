a9ff05a41ccda2253749d46861202184c48af185
Sheng Xiao <sheng26v@gmail.com>
2020-07-04 17:23:53 -0700

    chore: instead of using map getters directly access getters from store

--- src/views/Rules.vue
+++ src/views/Rules.vue
@@ -4,22 +4,16 @@
     description="The blow quiz sets will help you learn and practice knowledge
       of road rules.On the actual written test, you are reuqired to correctly
       answer at least 90% of the questions."
-    :questions="ruleQuestions"
+    :questions="$store.getters.ruleQuestions"
   />
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
 import TestPanel from '@/components/TestPanel.vue';
 
 export default {
   components: {
     TestPanel,
-  },
-  computed: {
-    ...mapGetters([
-      'ruleQuestions',
-    ]),
   },
 };
 </script>
--- src/views/Signs.vue
+++ src/views/Signs.vue
@@ -4,23 +4,17 @@
     description="The blow quiz sets will help you learn and practice knowledge
       of road signs.On the actual written test, you are reuqired to correctly
       answer at least 90% of the questions."
-    :questions="signQuestions"
+    :questions="$store.getters.signQuestions"
     :questionSetSize="25"
   />
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
 import TestPanel from '@/components/TestPanel.vue';
 
 export default {
   components: {
     TestPanel,
-  },
-  computed: {
-    ...mapGetters([
-      'signQuestions',
-    ]),
   },
 };
 </script>
